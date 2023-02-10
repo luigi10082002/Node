2a0b80e5a3c00598d1c293ab70866fbfc51370fc
luigi10082002 <luigimatheus55@gmail.com>
2023-02-10 10:21:59 -0300

    feat(aulas-parte-8): aulas 1 e 2 da parte 8 do curso de NodeJs

--- 8_Node_MySQL/2_inserindo_dados/index.js
+++ 8_Node_MySQL/2_inserindo_dados/index.js
@@ -4,15 +4,40 @@ const mysql = require('mysql')
 
 const app = express();
 
+
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+)
+
+app.use(express.json())
+
 app.engine('handlebars', exphbs.engine())
 
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 
-
 app.get('/', (req, res) => {
   res.render('home')
+})
+
+app.post('/books/insertbook', (req, res) => {
+
+  const title = req.body.title
+  const pageqty = req.body.pageqty
+
+  const sql = `INSERT INTO books (title, pageqty) VALUES ('${title}', '${pageqty}')`
+
+  conn.query(sql, function(err){
+  
+    if (err) {
+      console.log(err)
+    }
+  
+    res.redirect('/')
+  })
 })
 
 const conn = mysql.createConnection({
